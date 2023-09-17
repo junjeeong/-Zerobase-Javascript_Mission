c119c672368c0dceb285ca075d072a59d8a088fb
jeong-jun-yeong <wn8624@naver.com>
2023-09-17 16:40:04 +0900

    callendar-complete

--- Mission4/calendar.js
+++ Mission4/calendar.js
@@ -1,48 +1,71 @@
 const currentDay = document.getElementsByClassName('current-date')[0],
     days = document.getElementsByClassName('days')[0],
-    prevNextIcon = document.querySelectorAll('button');    
+    prevNextIcon = document.querySelectorAll('button');
 
 let date = new Date();
 let currYear = date.getFullYear(),
     currMonth = date.getMonth(),
-    months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
+    currDay = date.getDate(),
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const renderCalendar = () => {
-    const firstDateofMonth = new Date(currYear, currMonth, 1).getDate(),
-        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-        lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
-        lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+    const firstDateofMonth = new Date(currYear, currMonth, 1).getDay(),
+        lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
     let liTag = '';
 
-    for (let i = firstDateofMonth; i > 0; i--) {
-        liTag += `<li class = "inactive">${lastDateofLastMonth-i + 1 }</li>`;
+    for (let i = 0; i < firstDateofMonth; i++) {
+        liTag += `<li class="inactive">${new Date(currYear, currMonth, -i).getDate()}</li>`;
     }
- 
+
     for (let i = 1; i <= lastDateofMonth; i++) {
         let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class = "${isToday}">${i}</li>`;
-    }
- 
-    for (let i = lastDayofMonth; i < 6; i++) {
-        liTag += `<li class = "inactive">${i - lastDayofMonth + 1}</li>`;
+
+        if (new Date(currYear, currMonth, i).getDay() === 0) {
+            liTag += `<li class="sunday ${isToday}" data-year="${currYear}" data-month="${currMonth}" data-day="${i}">${i}</li>`;
+        } else {
+            liTag += `<li class="${isToday}" data-year="${currYear}" data-month="${currMonth}" data-day="${i}">${i}</li>`;
+        }
     }
 
     currentDay.innerHTML = `${months[currMonth]} ${currYear}`;
     days.innerHTML = liTag;
+
+    // 클릭 이벤트 리스너 추가
+    const dayElements = days.querySelectorAll('li');
+    dayElements.forEach((dayElement) => {
+        dayElement.addEventListener('click', () => {
+            const year = dayElement.getAttribute('data-year');
+            const month = dayElement.getAttribute('data-month');
+            const day = dayElement.getAttribute('data-day');
+            showDate(year, month, day);
+        });
+    });
 }
+
+function showDate(year, month, day) {
+    month = Number(month) + 1;
+    console.log(`${year}-${month}-${day}`);
+}
+
 renderCalendar();
 
 prevNextIcon.forEach((icon) => {
     icon.addEventListener('click', () => {
-    currMonth = icon.id === 'prev' ? currMonth - 1 : currMonth + 1;
-
-        if (currYear < 0 || currYear > 11) {
-            date = new Date(currYear, currMonth);
-            currYear = date.getFullYear();
-            currMonth = date.getMonth();
+        if (icon.id === 'prev') {
+            currMonth--;
+            if (currMonth < 0) {
+                currYear--;
+                currMonth = 11;
+            }
         } else {
-            date = new Date();
+            currMonth++;
+            if (currMonth > 11) {
+                currYear++;
+                currMonth = 0;
+            }
         }
-    renderCalendar();
+
+        date = new Date(currYear, currMonth, currDay);
+        renderCalendar();
     });
 });
